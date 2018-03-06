fed8aced80e6aa57fb8e311d5f828ee7d35d4b1c
Mary Warrick <mewarrick@gmail.com>
2018-03-06 11:48:12 -0600

    added tweetBank.js

--- app.js
+++ app.js
@@ -1,10 +1,30 @@
 
+
 const express = require( 'express' );
+const chalk = require('chalk');
+const morgan = require('morgan');
+const nunjucks = require('nunjucks');
 const app = express();
 
-//app.get('/', (req, req) => res.send('hello world!'))
 
-app.get('/', (req, res) => res.send('Hello World!'))
+const people = [{name: 'Full'}, {name: 'Stacker'}, {name: 'Son'}];
+var locals = {
+    title: 'An Example',
+    people: [
+        { name: 'Gandalf'},
+        { name: 'Frodo' },
+        { name: 'Hermione'}
+    ]
+};
+
+    
+    
+    
+app.set('view engine', 'html'); 
+app.engine('html', nunjucks.render); 
+nunjucks.configure('views', { noCache: true }); 
+
+app.get('/', (req, res, next) => {res.render( 'index', locals ); next()})
 
 
-app.listen(3000, () => console.log('Example app listening on port 3000!'))
+app.listen(3000, () => console.log('example app listening on port 3000'));
